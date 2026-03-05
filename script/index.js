1332cd698f5cf4140aba4c50878b736de6d2cac1
dinislamj6 <dinislamj6@gmail.com>
2026-03-05 14:22:44 +0600

    added functionality section

---
+++ script/index.js
@@ -0,0 +1,207 @@
+
+
+const createElement = (arr) => {
+    const htmlElements = arr.map(el => `<span class="btn">${el}</span>`)
+    return(htmlElements.join(" "))
+}
+
+const manageSpinner = (status) => {
+    if(status == true){
+        document.getElementById('spaner').classList.remove('hidden')
+        document.getElementById('word-container').classList.add('hidden')
+    }else{
+       document.getElementById('spaner').classList.add('hidden')
+        document.getElementById('word-container').classList.remove('hidden') 
+    }
+}
+
+const loadLessons = () => {
+    fetch('https://openapi.programming-hero.com/api/levels/all') // promise of response
+    .then(res => res.json()) // promise of json data 
+    .then(json => {
+        console.log(json.data);
+        displayLessons(json.data)
+    }) 
+}
+
+const removeActive = () => {
+    const lessonsButtons = document.querySelectorAll(".lesson-btn")
+    // lessonsButtons.classList.remove = "active"
+    lessonsButtons.forEach((btn) => btn.classList.remove('active'))
+}
+
+
+
+
+
+const loadLevelWord = (id) => {
+    manageSpinner(true)
+    
+     const url = `https://openapi.programming-hero.com/api/level/${id}`
+     fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        removeActive()
+        const clickBtn = document.getElementById(`lessons-btn-${id}`)
+        // console.log(clickBtn)
+        clickBtn.classList.add('active');
+
+        displayLevelWord(data.data)
+      })
+}
+
+const loadWordDetails = async(id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`
+    console.log(url)
+    const res = await fetch(url)
+    const details = await res.json()
+    displayWordDetails(details.data)
+
+}
+
+
+// "data": {
+// "word": "Brisk",
+// "meaning": "চটপটে / দ্রুত",
+// "pronunciation": "ব্রিস্ক",
+// "level": 3,
+// "sentence": "He took a brisk walk in the morning.",
+// "points": 3,
+// "partsOfSpeech": "adjective",
+// "synonyms": [
+// "quick",
+// "energetic"
+// ],
+// "id": 27
+// }
+
+const displayWordDetails = (word) => {
+    console.log(word)
+    const detailsBox = document.getElementById('details-container')
+    detailsBox.innerHTML = `
+     <div>
+        <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i>   :${word.pronunciation})</h2>
+        
+    </div>
+    <div>
+        <h2 class=" font-bold">Meaning</h2>
+        <p>${word.meaning}</p>
+    </div>
+    <div>
+        <h2 class=" font-bold">Example</h2>
+        <p>${word.sentence}</p>
+    </div>
+    <div>
+        <h2 class=" font-bold">সমার্থক শব্দ গুলো</h2>
+        <div class=""> 
+        ${createElement(word.synonyms)}
+        </div>
+    </div>
+    `
+
+  document.getElementById('word_modal').showModal()
+}
+
+
+
+
+
+
+
+
+
+
+const displayLevelWord = (words) => {
+    //   get the container end empty
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML = '' ;
+
+
+
+        if(words.length == 0){
+        wordContainer.innerHTML = `   
+        <div class="text-center col-span-full rounded-xl py-10 space-y-6 font-bangla">
+        <img class = "mx-auto" src="assets/alert-error.png" />
+        <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+    </div>  
+        `
+        manageSpinner(false)
+        return 
+       }
+       
+
+    for(const word of words){
+        console.log(word)
+        // new element create 
+        const card = document.createElement('div');
+        card.innerHTML = ` 
+             <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word ? word.word: "শব্দ পাওয়া যায়নি"}</h2>
+            <p class="font-semibold">Meaning /Pronunciation</p>
+            <div class="text-2xl font-medium font-bangla" >"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation? word.pronunciation: "pronunciation পাওয়া যায়নি"}"</div>
+            <div class="flex justify-between items-center">
+                <button onclick="loadWordDetails(${word.id})" class="btn bg-[#1a90ff3d] hover:bg-[#1a90ffd1]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1a90ff3d] hover:bg-[#1a90ffd1]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `
+        wordContainer.append(card)
+
+    }
+     manageSpinner(false)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const displayLessons = (lessons) => {
+    //1 : get container and empty
+    const levelContainer = document.getElementById('level-container');
+    levelContainer.innerHTML = '';
+
+    // get into every lesson 
+ for(const lesson of lessons){
+    console.log(lesson)
+     // new element create
+    const btnDiv = document.createElement('div');
+    btnDiv.innerHTML = ` 
+       <button id="lessons-btn-${lesson.level_no}" onClick="loadLevelWord(${lesson.level_no})" class= "btn btn-outline btn-primary lesson-btn">
+       <i class="fa-solid fa-book-open"></i>Lesson -${lesson.level_no}</button>
+    `
+    // append in container 
+    levelContainer.append(btnDiv)
+    
+ }
+       
+}
+loadLessons()
+
+
+document.getElementById('btn-search').addEventListener('click' , ()=>{
+    removeActive()
+    const input = document.getElementById('input-search');
+    const searchValue = input.value.trim().toLowerCase() 
+    console.log(searchValue)
+
+    fetch('https://openapi.programming-hero.com/api/words/all')
+    .then(res => res.json())
+    .then(data => {
+        const allWords = data.data
+        console.log(allWords)
+        const filterWord = allWords.filter(word => word.word.toLowerCase().includes(searchValue));
+
+        displayLevelWord(filterWord)
+      
+    })
+})
